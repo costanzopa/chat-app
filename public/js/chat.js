c7dfccda7f07a450b85f37805197ab22fa79c9f6
Pablo Costanzo <costanzopa@gmail.com>
2019-04-06 15:47:39 -0300

    Adding time and locations to the chat.

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,34 +1,46 @@
-const socket = io()
+const socket = io();
 
 // Elements
-const $messageForm = document.querySelector('#message-form')
-const $messageFormInput = $messageForm.querySelector('input')
-const $messageFormButton = $messageForm.querySelector('button')
-const $sendLocationButton = document.querySelector('#send-location')
-const $messages = document.querySelector('#messages')
+const $messageForm = document.querySelector('#message-form');
+const $messageFormInput = $messageForm.querySelector('input');
+const $messageFormButton = $messageForm.querySelector('button');
+const $sendLocationButton = document.querySelector('#send-location');
+const $messages = document.querySelector('#messages');
 
 // Templates
-const messageTemplate = document.querySelector('#message-template').innerHTML
+const messageTemplate = document.querySelector('#message-template').innerHTML;
+const locationMsgTemplate = document.querySelector('#location-message-template').innerHTML;
 
 socket.on('message', (message) => {
-    console.log(message)
+    console.log(message);
     const html = Mustache.render(messageTemplate, {
-        message
-    })
-    $messages.insertAdjacentHTML('beforeend', html)
-})
+        message: message.text,
+        createdAt: moment(message.createdAt).format('kk:m')
+    });
+    $messages.insertAdjacentHTML('beforeend', html);
+});
+
+socket.on('locationMessage', (locationMessage) => {
+    console.log(locationMessage);
+    const html = Mustache.render(locationMsgTemplate, {
+        url: locationMessage.url,
+        createdAt: moment(locationMessage.createdAt).format('kk:m')
+
+    });
+    $messages.insertAdjacentHTML('beforeend', html);
+});
 
 $messageForm.addEventListener('submit', (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    $messageFormButton.setAttribute('disabled', 'disabled')
+    $messageFormButton.setAttribute('disabled', 'disabled');
 
-    const message = e.target.elements.message.value
+    const message = e.target.elements.message.value;
 
     socket.emit('sendMessage', message, (error) => {
-        $messageFormButton.removeAttribute('disabled')
-        $messageFormInput.value = ''
-        $messageFormInput.focus()
+        $messageFormButton.removeAttribute('disabled');
+        $messageFormInput.value = '';
+        $messageFormInput.focus();
 
         if (error) {
             return console.log(error)
@@ -36,14 +48,14 @@ $messageForm.addEventListener('submit', (e) => {
 
         console.log('Message delivered!')
     })
-})
+});
 
 $sendLocationButton.addEventListener('click', () => {
     if (!navigator.geolocation) {
         return alert('Geolocation is not supported by your browser.')
     }
 
-    $sendLocationButton.setAttribute('disabled', 'disabled')
+    $sendLocationButton.setAttribute('disabled', 'disabled');
 
     navigator.geolocation.getCurrentPosition((position) => {
         socket.emit('sendLocation', {
